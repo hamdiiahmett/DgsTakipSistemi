31c3b55de43c1d6e2b37101d09c1d503716ad4c3
Ahmet Hamdi Oğuz <ahoo00881@icloud.com>
2026-03-26 16:47:48 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DgsTakipSistemi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // İleride kendi tablolarımızı buraya DbSet olarak ekleyeceğiz.
+    }
+}
